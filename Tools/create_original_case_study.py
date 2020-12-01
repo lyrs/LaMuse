9d573f527bd8bb6c8ba0ae42ec8123f93d5ee207
LAMIROY Bart <Bart.Lamiroy@univ-reims.fr>
2020-12-01 13:34:15 +0100

    Code refactoring and beautification

--- Tools/create_original_case_study.py
+++ Tools/create_original_case_study.py
@@ -1,9 +1,11 @@
 import os
 
-#os.chdir('./Mask_RCNN')
-import tensorflow.compat.v1 as tf
+# os.chdir('./Mask_RCNN')
+from glob import glob
 
-tf.disable_v2_behavior()
+# import tensorflow.compat.v1 as tf
+
+# tf.disable_v2_behavior()
 
 import random
 import cv2
@@ -13,11 +15,20 @@ from PIL import Image
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
 
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+
 from Tools.MaskRCNNModel import MaskRCNNModel
+
 
 # draw an image with detected objects
 
-def draw_image_with_boxes(filename, boxes_list):
+def draw_image_with_boxes(filename: str, boxes_list: list) -> None:
+    """
+    :param filename:
+    :param boxes_list:
+    :return:
+    """
     # load the image
     data = pyplot.imread(filename)
     # plot the image
@@ -38,96 +49,103 @@ def draw_image_with_boxes(filename, boxes_list):
     pyplot.show()
 
 
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+def create_case_study(path_to_paintings: str, path_objects_to_replace: str,
+                      path_to_background_images: str,
+                      path_to_results: str,
+                      nb_paintings: int = 5) -> None:
+    """
+    :param path_to_paintings:
+    :param path_objects_to_replace:
+    :param path_to_background_images:
+    :param path_to_results:
+    :param nb_paintings:
+    :return:
+    """
+    path_to_results += '/'
 
-
-def create_case_study(path_paintings, path_objects_to_replace, path_images_fond, path_resultats):
-    if not os.path.exists(path_resultats):
-        os.mkdir(path_resultats)
+    if not os.path.exists(path_to_results):
+        os.mkdir(path_to_results)
 
     # Run the model on the painting.
-    #@Todo find a way to invoke MaskRCNNModel only once
+    # @Todo find a way to invoke MaskRCNNModel only once
     model = MaskRCNNModel().model
 
-    list_paintings = os.listdir(path_paintings)
-    if '.DS_Store' in list_paintings:
-        list_paintings.remove('.DS_Store')
+    image_extensions = ["jpg", "gif", "png", "tga"]
+    painting_file_list = [y for x in [glob(path_to_paintings + '/*.%s' % ext) for ext in image_extensions] for y in x]
 
-    for painting_path in list_paintings:
-        painting = load_img(path_paintings + painting_path)
+    # List of available background images
+    background_file_list = \
+        [y for x in [glob(path_to_background_images + '/*.%s' % ext) for ext in image_extensions] for y in x]
+
+    object_file_list = {}
+    # List of candidate replacement objects
+    for obj in MaskRCNNModel.class_names:
+        object_file_list[obj] = [y for x in [glob(path_objects_to_replace + '/%s/*.%s' % (obj, ext))
+                                             for ext in image_extensions] for y in x]
+    for painting_filename in painting_file_list:
+
+        painting_name = os.path.basename(painting_filename)
+        painting = load_img(painting_filename)
         painting = img_to_array(painting)
-
-        # make prediction
-
-        results = model.detect([painting], verbose=0, probability_criteria=0.7)
-
-        r = results[0]
-
-        # Get the background image 
-
-        ## Random choice
-        list_background_images = os.listdir(path_images_fond)
-        if '.DS_Store' in list_background_images:
-            list_background_images.remove('.DS_Store')
-
-        # Resize the background image with the size of the painting.
-
         painting_width, painting_height = painting.shape[1], painting.shape[0]
 
-        # Parcours des objets reconnus et segmentés.
+        # Extract significant items from painting
+        results = model.detect([painting], verbose=0, probability_criteria=0.7)
+        r = results[0]
 
-        NUMBER_OF_TRIES = 10
+        # Generate a number of altered forms of painting
+        NUMBER_OF_TRIES = nb_paintings
 
-        ## List of Objects to replace.
-        list_objects = os.listdir(path_objects_to_replace)
-        if '.DS_Store' in list_objects:
-            list_objects.remove('.DS_Store')
         for j in range(NUMBER_OF_TRIES):
-            ## On change d'image de fond à tous les essais
-            random_path = random.choice(list_background_images)
-
-            background_image = Image.open(path_images_fond + '/' + random_path)
+            # Pick a random background image
+            background_image_name = random.choice(background_file_list)
+            background_image = Image.open(background_image_name)
+            # Resize the background image with the size of painting.
             background_image = background_image.resize((painting_width, painting_height), Image.ANTIALIAS)
+            background_image = background_image.convert("RGBA")
+
+            # file_saved = path_to_results + painting_name + str(j) + '.png'
+            # background_image.save(file_saved)
 
             for i in range(r['class_ids'].size):
-                ## On doit enregistrer l'image à toutes les itérations car la fonction 
-                ## putpixel ne fonctionne qu'après avoir sauvegardé les changements apportés
-                ## Ainsi on charge l'image temporaire, dans les cas où l'itération courante n'est pas la première 
-                if i != 0:
-                    background_image = Image.open(path_resultats + 'temp.png')
+                ##
+                # On doit enregistrer l'image à toutes les itérations car la fonction
+                # putpixel ne fonctionne qu'après avoir sauvegardé les changements apportés
+                # Ainsi on charge l'image temporaire, dans les cas où l'itération courante n'est pas la première
+                ##
+                current_class = MaskRCNNModel.class_names[r['class_ids'][i]]
 
-                ## Objet à coller
-                object_to_replace = Image.open(path_objects_to_replace + random.choice(list_objects))
+                # if i != 0:
+                #    background_image = Image.open(path_to_results + 'temp.png')
 
-                ## Définition des dimensions et du placement du futur objet à coller
+                # Pick a random object
+                replacement_object = Image.open(random.choice(object_file_list[current_class]))
 
-                boxes_img_to_replace = (r['rois'][i][1], r['rois'][i][0], r['rois'][i][3], r['rois'][i][2])
-                width_img_to_replace = boxes_img_to_replace[2] - boxes_img_to_replace[0]
-                height_img_to_replace = boxes_img_to_replace[3] - boxes_img_to_replace[1]
+                # Définition des dimensions et du placement du futur objet à coller
+                original_object_bbox = (r['rois'][i][1], r['rois'][i][0], r['rois'][i][3], r['rois'][i][2])
+                original_object_width = original_object_bbox[2] - original_object_bbox[0]
+                original_object_height = original_object_bbox[3] - original_object_bbox[1]
 
-                object_to_replace = object_to_replace.resize((width_img_to_replace, height_img_to_replace),
-                                                             Image.ANTIALIAS)
+                # Resizing replacement_object to original_object dimensions.
+                # @Todo: respect aspect ratio of replacement object
+                replacement_object = replacement_object.resize((original_object_width, original_object_height),
+                                                               Image.ANTIALIAS)
 
-                ## Collage de l'image dans la peinture.
+                # Paste replacement_object into background_image using alpha channel
+                background_image.paste(replacement_object, (r['rois'][i][1], r['rois'][i][0]), replacement_object)
 
-                for width in range(width_img_to_replace):
-                    for height in range(height_img_to_replace):
-                        value = object_to_replace.getpixel((width, height))
-                        if value != (0, 0, 0, 0):
-                            background_image.putpixel(
-                                (boxes_img_to_replace[0] + width, boxes_img_to_replace[1] + height), value)
-                # Save image.
+                # Save background_image.
                 if i == r['class_ids'].size - 1:
-                    file_saved = path_resultats + painting_path + str(j) + '.png'
+                    file_saved = path_to_results + painting_name + str(j) + '.png'
+                    background_image = background_image.convert("RGB")
                     background_image.save(file_saved)
-
-                else:
-                    background_image.save(path_resultats + 'temp.png')
-
-                # 
+                # else:
+                #    background_image.save(path_to_results + 'temp.png')
 
 
 if __name__ == "__main__":
-    create_case_study('./Paintings/', './CroppedAndVectorizedImages/animaux/', './BaseImages/paysages décors fonds',
-                      './NewRésultats2/')
+    painting_dir = './Paintings/'
+    background_dir = './BaseImages/paysages décors fonds'
+    output_dir = './NewRésultats2/'
+
+    create_case_study(painting_dir, './CroppedAndVectorizedImages/animaux/', background_dir, output_dir)
