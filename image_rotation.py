74c2562d7af70fc79f0ee27fd30f4092c33e997a
Firas <firas.abouda@telecomnancy.eu>
2021-03-17 16:42:54 +0100

    modification rotation d'image

--- image_rotation.py
+++ image_rotation.py
@@ -12,12 +12,25 @@ def getcontour(image):
     img_gray = cv2.cvtColor(blackAndWhitePNG(image), cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
     contour, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # contour of the target
+
+    print("Number of Contours found = " + str(len(contour)))
+    for i in (0,len(contour)-1):
+        print(contour[i].shape)
+
+    # Draw all contours
+    # -1 signifies drawing all contours
+    cv2.drawContours(image, contour, -1, (0, 255, 0), 3)
+
+    cv2.imshow('Contours', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     return contour
 
 
 if __name__ == "__main__":
-    default_image = cv2.imread('./LaMuse/rotate_image_testim/186_0d.png', cv2.IMREAD_UNCHANGED)
-    rotated_image = cv2.imread('./LaMuse/rotate_image_testim/186_45d.png', cv2.IMREAD_UNCHANGED)
+    default_image = cv2.imread('./LaMuse/rotate_image_testim/helicoptere/186_0d.png', cv2.IMREAD_UNCHANGED)
+    rotated_image = cv2.imread('./LaMuse/rotate_image_testim/helicoptere/186_45d.png', cv2.IMREAD_UNCHANGED)
 
     cd = getcontour(default_image)
     finale_image = applyOrientation(cd, getcontour(rotated_image), default_image)
